4f899922a4871fd9968d10102de0f1f8aa8516fd
mariocova3 <cm10079253@011-a218-37582.lisd.local>
2016-05-16 14:33:35 -0500

    Finished P1

---
+++ KeepUpGameP1/Assets/MouseFollow.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseFollow : MonoBehaviour
+{
+	float smooth = 5.0f;
+	float tiltAngle = 30.0f;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		float halfW = Screen.width / 2;
+		float halfH = Screen.height / 2;
+		transform.position = new Vector3 ((Input.mousePosition.x -halfW) / halfW, transform.position.y, (Input.mousePosition.y - halfH)/ halfH);
+
+		// Smoothly tilts a transform towards a target rotation.
+		float tiltAroundZ = Input.GetAxis("Mouse X") * tiltAngle * 2;
+		float tiltAroundX = Input.GetAxis("Mouse Y") * tiltAngle * -2;
+		Quaternion target = Quaternion.Euler (tiltAroundX, 0, tiltAroundZ);
+
+		// Dampen towards the target rotation
+		transform.rotation = Quaternion.Slerp(transform.rotation, target,Time.deltaTime * smooth);
+		}
+}
